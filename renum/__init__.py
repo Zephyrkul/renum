04f630916b667abe5137c8403e08a094e6ef8290
zephyrkul <zephyrkul@users.noreply.github.com>
2024-04-11 15:47:24 -0500

    initial commit

---
+++ renum/__init__.py
@@ -0,0 +1,43 @@
+# MIT License
+
+# Copyright (c) 2024 - 2024
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import sys as _sys
+
+from .renum import renum as renum
+
+__all__ = ("renum",)
+
+if _sys.version_info < (3, 8):
+    from importlib_metadata import metadata as _metadata
+else:
+    from importlib.metadata import metadata as _metadata
+
+__copyright__ = "Copyright 2019-2023 Zephyrkul"
+
+_meta = _metadata(__name__)
+__title__ = _meta["Name"]
+__author__ = _meta["Author"]
+__license__ = _meta["License"]
+__version__ = _meta["Version"]
+
+
+del _sys, _metadata, _meta  # not for export
---
+++ renum/renum.py
@@ -0,0 +1,120 @@
+from __future__ import annotations
+
+import re
+import sys
+from contextvars import ContextVar
+from enum import Enum
+from typing import TYPE_CHECKING, Any, ClassVar, Iterator, overload
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
+
+
+__all__ = ("renum",)
+_matches: ContextVar[dict[renum, re.Match[str]]] = ContextVar("_match")
+
+
+class renum(str, Enum):
+    _pattern_: ClassVar[re.Pattern[str]]
+
+    @staticmethod
+    def _generate_next_value_(
+        name: str, start: int, count: int, last_values: list[str]
+    ) -> str:
+        raise TypeError("enum.auto() used with renum")
+
+    def __new__(cls, *values: Any) -> Self:
+        value = str(*values)
+        member = str.__new__(cls, value)
+        member._value_ = value
+        return member
+
+    def __init_subclass__(cls, flags: int | re.RegexFlag = 0, **kwargs: Any) -> None:
+        super().__init_subclass__(**kwargs)
+        cls._pattern_ = re.compile(
+            r"|".join(rf"(?P<{member.name}>{member.value})" for member in cls),
+            flags=flags,
+        )
+
+    __str__ = str.__str__  # type: ignore
+    __format__ = str.__format__  # type: ignore
+
+    @property
+    def last_match(self) -> re.Match[str] | None:
+        """
+        The last re.Match that matched this renum member's pattern.
+        """
+        return _matches.get({}).get(self)
+
+    @property
+    def pattern(self) -> re.Pattern[str]:
+        """
+        The compiled re.Pattern for this renum class.
+        """
+        return self.__class__._pattern_
+
+    @overload
+    @classmethod
+    def _from_match(cls, match: None) -> None: ...
+    @overload
+    @classmethod
+    def _from_match(cls, match: re.Match[str]) -> Self: ...
+
+    @classmethod
+    def _from_match(cls, match: re.Match[str] | None) -> Self | None:
+        if not match:
+            return None
+        matched_groups = match.groupdict()
+        self = next(member for member in cls if matched_groups[member.name] is not None)
+        matches = _matches.get({})
+        matches[self] = match
+        _matches.set(matches)
+        return self
+
+    @classmethod
+    def search(
+        cls, string: str, pos: int = 0, endpos: int = sys.maxsize
+    ) -> Self | None:
+        """
+        Searches the specified string for an instance of this renum class,
+        as per the standard lib's re.search function.
+        """
+        return cls._from_match(cls._pattern_.search(string, pos, endpos))
+
+    @classmethod
+    def match(cls, string: str, pos: int = 0, endpos: int = sys.maxsize) -> Self | None:
+        """
+        Searches the specified string for an instance of this renum class,
+        as per the standard lib's re.match function.
+        """
+        return cls._from_match(cls._pattern_.match(string, pos, endpos))
+
+    @classmethod
+    def fullmatch(
+        cls, string: str, pos: int = 0, endpos: int = sys.maxsize
+    ) -> Self | None:
+        """
+        Searches the specified string for an instance of this renum class,
+        as per the standard lib's re.fullmatch function.
+        """
+        return cls._from_match(cls._pattern_.fullmatch(string, pos, endpos))
+
+    @classmethod
+    def findall(
+        cls, string: str, pos: int = 0, endpos: int = sys.maxsize
+    ) -> list[Self]:
+        """
+        Searches the specified string for all instances of this renum class,
+        as per the standard lib's re.findall function.
+        """
+        return list(cls.finditer(string, pos, endpos))
+
+    @classmethod
+    def finditer(
+        cls, string: str, pos: int = 0, endpos: int = sys.maxsize
+    ) -> Iterator[Self]:
+        """
+        Searches the specified string for instances of this renum class,
+        as per the standard lib's re.finditer function.
+        """
+        return map(cls._from_match, cls._pattern_.finditer(string, pos, endpos))

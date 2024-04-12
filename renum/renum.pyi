46e553a460157288fa3c537449b0201e44689437
zephyrkul <zephyrkul@users.noreply.github.com>
2024-04-12 02:08:53 -0500

    trim down .pyi

--- renum/renum.pyi
+++ renum/renum.pyi
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from contextvars import ContextVar
 from enum import Enum, EnumMeta
 from typing import Any, Iterator
 from typing_extensions import Self
@@ -8,24 +7,12 @@ from typing_extensions import Self
 import regex
 
 __all__ = ("RenumType", "renum")
-_matches: ContextVar[dict[renum, regex.Match[str]]] = ContextVar("_match")
 
 class RenumType(EnumMeta):
-    """Metaclass for renum classes"""
-
-    _pattern_: regex.Pattern[str]
-
     @property
     def pattern(self) -> regex.Pattern[str]: ...
 
 class renum(Enum, metaclass=RenumType):
-    """
-    A utility class for generating Enum-like regular expression patterns.
-
-    Parameters:
-        flags (int | re.RegexFlag): Regular expression flags to pass to `re.compile`
-    """
-
     _value_: str
 
     @property
